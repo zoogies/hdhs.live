e7362f28b37201234e36404c4afc558f76967d2c
Yoyolick <ryanzmuda@gmail.com>
2021-10-25 18:22:18 -0400

    small tweaks

---
+++ src/dev/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo .sh files are completely scuffed for me rn and i know your ass is gonna forget this so here is how to start the server:
+echo export FLASK_APP=server
+echo export FLASK_ENV=development
+echo python3 -m flask run --host=192.168.50.77
