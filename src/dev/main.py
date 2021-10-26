64bc568384ad70caa0c3bc6ff1c4bd0465ca5a0a
Yoyolick <ryanzmuda@gmail.com>
2021-10-26 18:50:11 -0400

    scared of losing progress. very big moves

--- src/dev/main.py
+++ src/dev/main.py
@@ -1,9 +1,11 @@
 import os
 import sqlite3
 from flask import Flask, render_template
-from flask import g
+from flask import g, request
 
 DATABASE = 'danarchy.db'
+whitelist = ['192.168.50.1']
+#whitelist = []
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -19,6 +21,17 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+#RESTRICT ACCESS CODE:
+def verifyip(ip):
+    if ip[0:7] == "174.207":
+        return True
+    else:
+        if ip in whitelist:
+            return True
+        else:
+            return False
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -27,13 +40,16 @@ def create_app(test_config=None):
     @app.route('/')
     def main():
         cur = get_db().cursor()
-        return render_template('index.html') #https://realpython.com/primer-on-jinja-templating/
+        if verifyip(request.remote_addr):
+            return render_template('index.html') #https://realpython.com/primer-on-jinja-templating/
+        else:
+            return render_template('denied.html')
     @app.teardown_appcontext
     def close_connection(exception):
         db = getattr(g, '_database', None)
         if db is not None:
             db.close()
-    @app.route('/db')
+    @app.route('/dbprev')
     def db():
-        return query_db('select * from main')
+        return str(query_db('select * from main'))
     return app
---
+++ src/dev/static/js/main.js
@@ -0,0 +1,12 @@
+function reportmistake(){
+    alert('LMAO i didnt program this yet so tough luck i guess. find out who i am and let me know.');
+    //TODO
+}
+
+function init(){
+    var container = !!document.getElementById("container");
+    if (container) {
+        //run if we are on a page with the container
+        console.log('container detected');
+    }
+}
