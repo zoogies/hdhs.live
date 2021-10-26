ba6dae4f290862b725851233019e64cead5da630
Yoyolick <ryanzmuda@gmail.com>
2021-10-25 21:28:43 -0400

    i have literally 0 idea why my internet wont allow incoing wan requests over forwarded ports but it makes me want to smash my head against a wall until there is no part of my brain left to be able to worry about this stupid of a problem

--- src/dev/main.py
+++ src/dev/main.py
@@ -1,18 +1,23 @@
 import os
 import sqlite3
 from flask import Flask, render_template
-def dbop(arg: str):
-    try:
-        db = sqlite3.connect('danarchy.db')
-        cur = db.cursor()
-        return cur.execute(arg)
-    except:
-        db.rollback()
-        print('BIG L MASSIVE ERROR')
-    finally:
-        db.commit()
-        db.close()
-        print('SQL DATABASE OPERATION COMPLETE')
+from flask import g
+
+DATABASE = 'danarchy.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 def create_app(test_config=None):
     # create and configure the app
@@ -21,9 +26,14 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/')
     def main():
+        cur = get_db().cursor()
         return render_template('index.html') #https://realpython.com/primer-on-jinja-templating/
-
+    @app.teardown_appcontext
+    def close_connection(exception):
+        db = getattr(g, '_database', None)
+        if db is not None:
+            db.close()
     @app.route('/db')
     def db():
-        return dbop("SELECT * FROM main")
+        return query_db('select * from main')
     return app
