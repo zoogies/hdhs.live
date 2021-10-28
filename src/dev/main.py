901b0602a8531e435d6afc1113a730e221f60cec
Yoyolick <ryanzmuda@gmail.com>
2021-10-27 20:24:56 -0400

    Added ability to post and bought domain

--- src/dev/main.py
+++ src/dev/main.py
@@ -1,12 +1,15 @@
 import os
 import sqlite3
+import random
+import datetime
 import json
 from flask import Flask, render_template
 from flask import g, request
 
 DATABASE = 'danarchy.db'
-whitelist = ['192.168.50.1']
-#whitelist = []
+whitelist = ['192.168.50.1',"198.30.181.13","174.207.98.227"]
+
+#whitelist key: ryan macbook, xavier phone, ryan hotspot
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -14,7 +17,11 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-
+def execute_db(cmd):
+    con = sqlite3.connect(DATABASE)
+    c = con.cursor()
+    c.execute(cmd)
+    con.commit()
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -24,7 +31,7 @@ def query_db(query, args=(), one=False):
 
 #RESTRICT ACCESS CODE:
 def verifyip(ip):
-    if ip[0:7] == "174.207":
+    if ip[0:7] == "192.149":
         return True
     else:
         if ip in whitelist:
@@ -35,7 +42,7 @@ def verifyip(ip):
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True,)
 
     # a simple page that says hello
     @app.route('/')
@@ -45,17 +52,31 @@ def create_app(test_config=None):
             return render_template('index.html') #https://realpython.com/primer-on-jinja-templating/
         else:
             return render_template('denied.html')
+
     @app.teardown_appcontext
     def close_connection(exception):
         db = getattr(g, '_database', None)
         if db is not None:
             db.close()
+
     @app.route('/dbprev')
     def db():
         return str(query_db('select * from main'))
+
     @app.route('/fetchnew')
     def new():
         #print(json.dumps(query_db('select * from main')))
         #print(type(query_db('select * from main')))
         return json.dumps(query_db('select * from main'))
+    
+    @app.route('/post',methods=['GET', 'POST'])
+    def post():
+        req = request.json
+        #print('insert into main (ID,USER,CONTENT,LIKES,STAMP) values ('+str(random.randint(0,99999))+',"'+req['USER']+'","'+req['CONTENT']+'",'+ str(0)+',"'+str(datetime.datetime.now())[0:19]+'")')
+        execute_db('insert into main (ID,USER,CONTENT,LIKES,STAMP) values ('+str(query_db('SELECT Count(*) FROM main')[0][0])+',"'+req['USER']+'","'+req['CONTENT']+'",'+ str(0)+',"'+str(datetime.datetime.now())[0:19]+'")')
+        return "recieved"
     return app
+
+#TODO:
+#make website split to grid of columns of posts if width is high enough
+#google analytics
--- src/dev/static/js/main.js
+++ src/dev/static/js/main.js
@@ -2,8 +2,47 @@ function reportmistake(){
     alert('LMAO i didnt program this yet so tough luck i guess. find out who i am and let me know.');
     //TODO
 }
+
+function trackchar(){
+    var elem = document.getElementById("charlim");
+    elem.innerText = document.getElementById("field").value.length + "/250 characters";
+}
+
+function post(){
+    //TODO
+    //alert();
+    var posttext = document.getElementById("field").value;
+    if(posttext.length <= 250 && posttext.length > 0){
+        //alert(posttext);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://76.181.32.163:5000/post");
+
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            //console.log(xhr.status);
+            //console.log(xhr.responseText);
+        }};
+        now = new Date();
+        var data = {
+            "USER": "UNKNOWN",
+            "CONTENT": String(posttext),
+            //"STAMP": now.getHours() + ':' + now.getMinutes() + ' ' + now.getMonth() + '/' + now.getDate() + '/' + now.getFullYear()
+        };
+        //alert(JSON.stringify(data))
+        xhr.send(JSON.stringify(data));
+        location.reload();
+    }
+    else{
+        alert('Your post needs to be more than 0 and less than 250 characters')
+    }
+}
+
 //todo loading symbol until posts are loaded
 function init(){
+    //detect container
     var container = !!document.getElementById("container");
     if (container) {
         //run if we are on a page with the container
@@ -21,9 +60,13 @@ function init(){
                 header = '<div class="post"><div class="p_header"><img class="icon spaced" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/><p>'+cs[i][1]+'</p><p class="ID">'+'#'+cs[i][0]+'</p><p class="ID">'+cs[i][5]+'</p></div><p class="spaced">'+cs[i][2]+'</p><div class="p_footer"><p class="spaced">'+cs[i][4]+' Likes'+'</p><p class="spaced"><b>Click For Comments</b></p></div></div>'
                 document.getElementById("container").innerHTML += header;
                 
-                console.log(header)
+                //console.log(header)
             }
         }
         });
+        var ld = document.getElementById("loading");
+        ld.remove();
+
+        var chartrack=setInterval(trackchar,100);
     }
 }
