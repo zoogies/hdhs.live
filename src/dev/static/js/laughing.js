683fdf415fb48d62f8824c6c03379a08d7301b98
Yoyolick <ryanzmuda@gmail.com>
2021-11-13 15:52:23 -0500

    laughing backend seperated from frontend

--- src/dev/static/js/laughing.js
+++ src/dev/static/js/laughing.js
@@ -2,24 +2,31 @@
 
 //laugh at normal post
 function laugh(id,type){
+    //update the display count only
+    if (type == 'post'){
+        //get new laugh num
+        var num = parseInt(document.getElementById(id).querySelector('#liketext').innerText.split(' ')[0]) + 1;
+        document.getElementById(id).querySelector('#liketext').innerText = num + ' Laughs';
+    }
+    else if (type == 'comment') {
+        //get new laugh num
+        var num = parseInt(document.getElementById('comment_'+id).innerText.split(' ')[0]) + 1;
+        document.getElementById('comment_' + id).innerText = num + ' Laughs';
+    }
+    else {
+        throw 'Laugh content type is invalid'
+    }
+    
     //set our data
     var data = {
         "id": String(id),
         "type": String(type)
     };
-
+    
     //send a xhr request using our async function we wrote and act based on the result
     basicxhr('laugh', data)
     .then(function (response) {
-        if (type == 'post'){
-            document.getElementById(id).querySelector('#liketext').innerText = response + ' Laughs';
-        }
-        else if (type == 'comment') {
-            document.getElementById('comment_' + id).innerText = response + ' Laughs';
-        }
-        else {
-            throw 'Laugh content type is invalid'
-        }
+        //blank lmao
     })
     .catch(function (err) {
         console.error('An error occured!', err.statusText);
