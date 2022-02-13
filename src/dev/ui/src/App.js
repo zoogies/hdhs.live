5409de7d9463f63eda149f8bbdc08365950ec02f
Ryan Zmuda <ryanzmuda@gmail.com>
2022-02-12 21:08:51 -0500

    back to react struggle

--- src/dev/ui/src/App.js
+++ src/dev/ui/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+function basicxhr(route,data){
+  return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://127.0.0.1:5000/"+route);
+      xhr.setRequestHeader("Accept","apllication/json");
+      xhr.setRequestHeader("Content-Type","application/json");
+      xhr.onload = function () {
+          if (this.status >= 200 && this.status < 300) {
+              resolve(xhr.response);
+          } 
+          else {
+              reject({
+                  status: this.status,
+                  statusText: xhr.statusText
+              });
+          }
+      };
+      xhr.onerror = function () {
+          //this is where its rejecting on mobile TODO HTTPS
+          reject({
+              status: this.status,
+              statusText: xhr.statusText
+          });
+      };
+      xhr.send(JSON.stringify(data));
+      });
+}
+
 function App() {
+  var data = {
+    "order": "new",
+    "start": "fresh",
+    "numloaded": String(0),
+  }
+  basicxhr('fetchposts',data).then(function (response) {
+    console.log(JSON.parse(response))
+    JSON.parse(response).map(function(post){
+      return(
+        <p>{post[2]}</p>
+      )
+    })
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="subcontainer">
+        <p>balls</p>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
